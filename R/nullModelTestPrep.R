544d736bb4629fcfcdc46e18b2584757905bb285
Stephanie M. Gogarten <sdmorris@uw.edu>
2018-04-16 11:32:11 -0700

    reorganize code for clarity
    Reduce duplicated code in nullModelTestPrep by combining the
    mixed model and unrelated hetResid cases. Code path now depends
    on whether cholSigmaInv is a scalar or a matrix. The prior case
    of treating a diagonal matrix differently by extracting the
    diagonal elements is no longer necessary, since operations with
    a Diagonal Matrix class in the Matrix package are just as fast.

--- R/nullModelTestPrep.R
+++ R/nullModelTestPrep.R
@@ -5,50 +5,33 @@ nullModelTestPrep <- function(nullmod){
     Y <- nullmod$workingY
     X <- Matrix(nullmod$model.matrix)
     
-    if (nullmod$family$mixedmodel){  ## n by n cholSigmaInv
+    if (nullmod$family$mixedmodel | (nullmod$family$family == "gaussian")){
         C <- nullmod$cholSigmaInv
-        CX <- crossprod(C, X)
+        if (length(C) > 1) { ## n by n cholSigmaInv (may be Diagonal)
+            CX <- crossprod(C, X)
+            CXCXI <- tcrossprod(CX, chol2inv(chol(crossprod(CX))))
+            CCXCXICX <- tcrossprod(tcrossprod(C, t(CXCXI)), CX)
+            Ytilde <- crossprod(C, Y) - crossprod(CCXCXICX, Y)
+            resid <- tcrossprod(C, t(Ytilde)) - tcrossprod(CCXCXICX, t(Ytilde))
+        } else { ## cholSigmaInv is a scalar
+            CX <- X * C 
+            CXCXI <- tcrossprod(CX, chol2inv(chol(crossprod(CX))))
+            CCXCXICX <- tcrossprod(CXCXI*C, CX)
+            Ytilde <- C*Y - crossprod(CCXCXICX, Y)
+            resid <- nullmod$resid.marginal/nullmod$varComp
+        }
     }
 
     if (!nullmod$family$mixedmodel & (nullmod$family$family != "gaussian")){
         sigma <- sqrt(nullmod$varComp)
         C <- Diagonal(x=sigma)
         CX <- X * sigma
-    }
-
-    if (!nullmod$family$mixedmodel & (nullmod$family$family == "gaussian")){
-        if (nullmod$hetResid) {  ## cholSigmaInv is diagonal
-            C <- diag(nullmod$cholSigmaInv)
-        } else { ## family is "gaussian", cholSigmaInv is a scalar.
-            C <- nullmod$cholSigmaInv
-        }
-        CX <- X * C      ## this is equal to crossprod(diag(C), X) when C is a vector  
-    }
-
-    CXCXI = tcrossprod(CX, chol2inv(chol(crossprod(CX))))
-    
-    if (nullmod$family$mixedmodel){
-        CCXCXICX <- tcrossprod(tcrossprod(C, t(CXCXI)), CX)
-        Ytilde <- crossprod(C, Y) - crossprod(CCXCXICX, Y)
-        resid <- tcrossprod(C, t(Ytilde)) - tcrossprod(CCXCXICX, t(Ytilde))
-    }
-    
-    if (!nullmod$family$mixedmodel & (nullmod$family$family != "gaussian")){
+        CXCXI <- tcrossprod(CX, chol2inv(chol(crossprod(CX))))
         CCXCXICX <- tcrossprod(tcrossprod(C, t(CXCXI)), CX)
         Ytilde <- crossprod(C, Y) - crossprod(CCXCXICX, Y)
         resid <- nullmod$resid.marginal
     }
-    
-    if (!nullmod$family$mixedmodel & (nullmod$family$family == "gaussian")){
-        CCXCXICX <- tcrossprod(CXCXI*C, CX)
-        Ytilde <- C*Y - crossprod(CCXCXICX, Y)
-      if (nullmod$hetResid){
-          resid <- as.vector(C*Ytilde - tcrossprod(CCXCXICX, t(Ytilde)))
-      } else{
-          resid <- nullmod$resid.marginal/nullmod$varComp
-      }
-    }
-    
+
     return(list(Ytilde=Ytilde, resid=resid, CX=CX, CXCXI=CXCXI))
 }
 
@@ -56,23 +39,21 @@ nullModelTestPrep <- function(nullmod){
 ##  adjust genotypes for correlation structure and fixed effects
 calcXtilde <- function(nullmod, G){
     
-    if (nullmod$family$mixedmodel){  ## n by n cholSigmaInv
-        M1 <- crossprod(nullmod$cholSigmaInv,G)
-    }
-    
-    if (!nullmod$family$mixedmodel & (nullmod$family$family != "gaussian")){
-        C <- Diagonal(x=sqrt(nullmod$varComp))
-        M1 <- crossprod(C,G)
-    }
-
-    if (!nullmod$family$mixedmodel & (nullmod$family$family == "gaussian")){
-        if (length(nullmod$cholSigmaInv) == 1) { # cholSigmaInv is a scalar
-            M1 <- G*nullmod$cholSigmaInv
-        } else {
-            M1 <- crossprod(nullmod$cholSigmaInv,G) # cholSigmaInv is a diagonal matrix
+    if (nullmod$family$mixedmodel | (nullmod$family$family == "gaussian")){
+        C <- nullmod$cholSigmaInv
+        if (length(C) > 1) { ## n by n cholSigmaInv (may be Diagonal)
+            M1 <- crossprod(C, G)
+        } else { ## cholSigmaInv is a scalar
+            M1 <- G * C
         }
     }
-    
+
+    if (!nullmod$family$mixedmodel & (nullmod$family$family != "gaussian")){
+        sigma <- sqrt(nullmod$varComp)
+        C <- Diagonal(x=sigma)
+        M1 <- crossprod(C, G)
+    }
+
     rm(G)
     Xtilde <- M1 - tcrossprod(nullmod$CXCXI, crossprod(M1, nullmod$CX))
     return(Xtilde)
