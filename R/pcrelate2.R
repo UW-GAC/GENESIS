76a3c3f8adce9c8948875c04bba164302cb9d497
Matthew Conomos <mconomos@D-10-19-46-161.dhcp4.washington.edu>
2018-09-28 15:21:43 -0700

    initial version of pcrelate2 complete; does not do any splitting or parallelization of samples or snps

--- R/pcrelate2.R
+++ R/pcrelate2.R
@@ -1,3 +1,101 @@
+pcrelate2 <- function(	gdsobj,
+						pcs,
+						scale = c('overall', 'variant', 'none'),
+						ibd.probs = TRUE,
+						sample.include = NULL,
+						training.set = NULL,
+						snp.include = NULL,
+						maf.thresh = 0.01,
+						maf.bound.method = c('truncate', 'filter'),
+						small.samp.correct = FALSE,
+						verbose = TRUE){
+
+	# some parameter checks
+	if(maf.thresh < 0 | maf.thresh > 0.5) stop('maf.thresh must be in [0,0.5]')
+	if(scale == 'none' & ibd.probs) stop('IBD probabilities can not be calculated when `scale` == none')
+	### more to add ###
+
+
+	# calculate betas for individual specific allele frequencies
+	### this may be done externally from this function first; in this case we would load betas below ###
+	if(!is.null(training.set)){
+		beta <- calcBetaISAF(gdsobj = gdsobj, pcs = pcs, sample.include = training.set, snp.include = snp.include)
+	}else{
+		beta <- calcBetaISAF(gdsobj = gdsobj, pcs = pcs, sample.include = sample.include, snp.include = snp.include)
+	}
+
+
+	# get PC matrix for current block of samples
+	V <- .createPCMatrix(gdsobj = gdsobj, pcs = pcs, sample.include = sample.include)
+	
+
+	# compute estimates for current block of samples and variants
+	### this could be parallelized by blocks of variants in snp.include ###
+
+	# load genotype data
+	seqSetFilter(gdsobj, variant.id = snp.include, sample.id = rownames(V))
+	G <- altDosage(gdsobj)
+	### rownames(V) needs to match rownames(G) - it should based on the .createPCMatrix() function
+
+	# load betas for the current block of variants (colnames(G))
+	beta.block <- beta[match(colnames(G), rownames(beta)), , drop = FALSE]
+	### this line of code will probably be different if we save the betas; need to load correct betas
+	### rownames of beta.block needs to match colnames of G
+
+	# estimate individual specific allele frequencies
+	mu <- .estISAF(V = V, beta = beta.block, bound.method = maf.bound.method, bound.thresh = maf.thresh)
+
+
+	# compute values for estimates
+	if(scale == 'overall'){
+		estList <- .pcrCalcOvr(G = G, mu = mu, ibd.probs = ibd.probs)
+	}else if(scale == 'variant'){
+		estList <- .pcrCalcVar(G = G, mu = mu, ibd.probs = ibd.probs)
+	}else if(scale == 'none'){
+		kin <- .pcrCalcNone(G = G, mu = mu)
+	}
+
+
+	### this needs to be done outside of the parallelization; all the matrices need to be summed first ###
+
+	# compute final estimates
+	if(scale == 'overall'){
+		kin <- estList$kinNum/(4*estList$kinDen)
+	}else if(scale == 'variant'){
+		kin <- estList$kin/(4*estList$nsnp)
+	}
+
+	if(ibd.probs){
+		if(scale == 'overall'){
+			k2 <- estList$k2Num/estList$k2Den
+			k0 <- estList$k0Num/estList$k0Den
+		}else if(scale == 'variant'){
+			k2 <- estList$k2/estList$nsnp
+			k0 <- estList$k0/estList$nsnp
+		}
+
+		# correct k2 for HW departure
+		f <- 2*diag(kin) - 1
+		fprod <- tcrossprod(f)
+		k2 <- k2 - fprod
+
+		# use alternate k0 estimator for non-1st degree relatives
+		k0.idx <- which(kin < 2^(-5/2))
+		k0[k0.idx] <- 1 - 4*kin[k0.idx] + k2[k0.idx]
+	}
+
+	### need to rewrite the small sample correction at some point - take care of this later ###
+
+
+	return(list(kin = kin, k0 = k0, k2 = k2))
+}
+
+
+
+
+
+### these functions are for computing PC betas for individual specific allele frequency calculations ###
+
 
 calcBetaISAF <- function(gdsobj, pcs, sample.include, snp.include){
 	# create matrix of PCs
@@ -9,17 +107,18 @@ calcBetaISAF <- function(gdsobj, pcs, sample.include, snp.include){
 
 	### this could be parallelized by blocks of variants in snp.include ###
 	# calculate betas for PCs at each variant
-	.calcBetaISAF(gdsobj, snp.include, VVtVi)
+	beta <- .calcBetaISAF(gdsobj = gdsobj, VVtVi = VVtVi, snp.include = snp.include)
+
+	return(beta)
 }
+
 
 .createPCMatrix <- function(gdsobj, pcs, sample.include){
 	# subset to sample.include
 	seqSetFilter(gdsobj, sample.id = sample.include)
 	sample.id <- seqGetData(gdsobj, 'sample.id')
 
-	if(!all(sample.id %in% rownames(pcs))){
-		stop('all samples must be in pcs')
-	}
+	if(!all(sample.id %in% rownames(pcs))) stop('all samples must be in pcs')
 
 	# subset and re-order pcs if needed
 	V <- pcs[match(sample.id, rownames(pcs)), , drop = FALSE]
@@ -31,40 +130,21 @@ calcBetaISAF <- function(gdsobj, pcs, sample.include, snp.include){
 
 .calcBetaISAF <- function(gdsobj, VVtVi, snp.include){
 	# load genotype data
-	seqSetFilter(gdsobj, variant.sel = snp.include)
+	seqSetFilter(gdsobj, variant.id = snp.include)
 	G <- altDosage(gdsobj)
 
 	# compute beta estimates
 	beta <- crossprod(G, VVtVi)
 	### beta is a matrix of variants x PCs; needs to be saved, not sure of the best format ###
+
+	return(beta)
 }
 
 
 
+### these functions are for estimating individual specific allele frequencies
 
-
-### kinship for one block of samples and one block of variants ###
-
-
-V <- .createPCMatrix(gdsobj = gdsobj, pcs = pcs, sample.include = sample.include)
-
-
-### this could be parallelized by blocks of variants in snp.include ###
-
-
-# load genotype data
-seqSetFilter(gdsobj, variant.sel = snp.include, sample.id = rownames(V))
-G <- altDosage(gdsobj)
-### rownames(V) needs to match rownames(G) - it should based on the .createPCMatrix() function
-
-# load betas for the current block of variants (colnames(G))
-beta.block <- # something
-### rownames of beta.block needs to match colnames of G
-
-# estimate individual specific allele frequencies
-mu <- .estISAF(V, beta.block, bound.method = bound.method, bound.thresh = bound.thresh)
-
-.estISAF <- function(V, beta, bound.method = c('truncate','filter'), bound.thresh){
+.estISAF <- function(V, beta, bound.method, bound.thresh){
 	# get ISAF estimates (i.e. 0.5*fitted values)
 	mu <- 0.5*tcrossprod(V, beta)
 	# fix boundary cases
@@ -93,117 +173,51 @@ mu <- .estISAF(V, beta.block, bound.method = bound.method, bound.thresh = bound.
 }
 
 
-if(scale != 'none'){
-	# calculate mu(1-mu)
+### all of the functions below are used for computing kinship and ibd estimates ###
+
+.pcrCalcOvr <- function(G, mu, ibd.probs){
+	# compute mu(1 - mu)
 	muqu <- mu*(1 - mu)
-}
 
-
-if(scale == 'variant'){
-	# compute number of overlapping snps by pair
-	nonmiss <- !(is.na(G) | is.na(mu))
-	nsnp <- crossprod(nonmiss)
-	# index of missing values
-	filt.idx <- which(!nonmiss)
-	rm(nonmiss)
-}else{
 	# index of missing values
 	filt.idx <- which(is.na(G) | is.na(mu))
-}
 
+	# compute kinship values
+	kinList <- .pcrCalcKinOvr(G, mu, muqu, filt.idx)
 
-filt.idx <- which(is.na(G) | is.na(mu))
+	if(ibd.probs){
+		# compute ibd values
+		ibdList <- .pcrCalcIBDOvr(G, mu, muqu, filt.idx)
 
+		return(list(kinNum = kinList$kinNum, 
+					kinDen = kinList$kinDen,
+					k0Num = ibdList$k0Num,
+					k0Den = ibdList$k0Den,
+					k2Num = ibdList$k2Num,
+					k2Den = ibdList$k2Den))
 
-
-
-
-# calculate kinship values for the block of variants
-if(scale = 'overall'){
-	kinList <- .calcKinOvr(G, mu, muqu, filt.idx)
-}else if(scale == 'variant'){
-	kin <- .calcKinVar(G, mu, muqu, filt.idx)
-}else if(scale == 'none'){
-	kin <- .calcKinNone(G, mu, filt.idx)
-}
-
-# calculate ibd values for the block of variants
-if(ibd.probs){
-	if(scale == 'overall'){
-		ibdList <- .calcIBDOvr(G, mu, muqu, filt.idx)
-	}else if(scale == 'variant'){
-		ibdList <- .calcIBDVar(G, mu, muqu, filt.idx)
+	}else{
+		return(kinList)
 	}
 }
 
-
-### this needs to be done outside of the parallelization; all the matrices need to be summed first ###
-
-# compute final estimates
-if(scale == 'overall'){
-	kin <- kinList$kinNum/(4*kinList$kinDen)
-}else if(scale == 'variant'){
-	kin <- kin/(4*nsnp)
-}
-
-if(ibd.probs){
-	if(scale == 'overall'){
-		k2 <- ibdList$k2Num/ibdList$k2Den
-		k0 <- ibdList$k0Num/ibdList$k0Den
-	}else if(scale == 'variant'){
-		k2 <- ibdList$k2/nsnp
-		k0 <- ibdList$k0/nsnp
-	}
-
-	# correct k2 for HW departure
-	f <- 2*diag(kin) - 1
-	fprod <- tcrossprod(f)
-	k2 <- k2 - fprod
-
-	# use alternate k0 estimator for non-1st degree relatives
-	k0.idx <- which(kin < 2^(-5/2))
-	k0[k0.idx] <- 1 - 4*kin[k0.idx] + k2[k0.idx]
-}
-
-
-
-.calcKinOvr <- function(G, mu, muqu, filt.idx){
+.pcrCalcKinOvr <- function(G, mu, muqu, filt.idx){
 	# residuals
-	R <- G - 2*mu	
+	R <- G - 2*mu
+
 	# set missing values to 0 (i.e. no contribution from that variant)
 	R[filt.idx] <- 0
 	muqu[filt.idx] <- 0
+
 	# numerator (crossprod of residuals)
 	kinNum <- tcrossprod(R)
-	# denominator (needs to be multipled by 2 when all put together)
+	# denominator
 	kinDen <- tcrossprod(sqrt(muqu))
 
 	return(list(kinNum = kinNum, kinDen = kinDen))
 }
 
-.calcKinVar <- function(G, mu, muqu, filt.idx){
-	# scaled residuals
-	R <- (G - 2*mu)/sqrt(muqu)
-	# set missing values to 0 (i.e. no contribution from that variant)
-	R[filt.idx] <- 0
-	# crossprod of scaled residuals
-	kin <- tcrossprod(R)
-
-	return(kin)
-}
-
-.calcKinNone <- function(G, mu, filt.idx){
-	# residuals
-	R <- G - 2*mu
-	# set missing values to 0 (i.e. no contribution from that variant)
-	R[filt.idx] <- 0
-	# crossprod of scaled residuals
-	kin <- tcrossprod(R)
-
-	return(kin)
-}
-
-.calcIBDOvr <- function(G, mu, muqu, filt.idx){
+.pcrCalcIBDOvr <- function(G, mu, muqu, filt.idx){
 	# opposite allele frequency
 	qu <- 1 - mu
 
@@ -236,7 +250,50 @@ if(ibd.probs){
 	return(list(k0Num = k0Num, k0Den = k0Den, k2Num = k2Num, k2Den = k2Den))
 }
 
-.calcIBDVar <- function(G, mu, muqu, filt.idx){
+
+.pcrCalcVar <- function(G, mu, ibd.probs){
+	# compute mu(1 - mu)
+	muqu <- mu*(1 - mu)
+
+	# compute number of observed snps by pair
+	nonmiss <- !(is.na(G) | is.na(mu))
+	nsnp <- tcrossprod(nonmiss)
+
+	# index of missing values
+	filt.idx <- which(!nonmiss)
+	rm(nonmiss)
+
+	# compute kinship values
+	kinNum <- .pcrCalcKinVar(G, mu, muqu, filt.idx)
+
+	if(ibd.probs){
+		# compute ibd values
+		ibdList <- .pcrCalcIBDVar(G, mu, muqu, filt.idx)
+
+		return(list(kinNum = kinNum,
+					k0Num = ibdList$k0Num,
+					k2Num = ibdList$k2Num,
+					nsnp = nsnp))
+
+	}else{
+		return(list(kinNum = kinNum, nsnp = nsnp))
+	}
+}
+
+.pcrCalcKinVar <- function(G, mu, muqu, filt.idx){
+	# scaled residuals
+	R <- (G - 2*mu)/sqrt(muqu)
+
+	# set missing values to 0 (i.e. no contribution from that variant)
+	R[filt.idx] <- 0
+
+	# numerator (crossprod of scaled residuals)
+	kinNum <- tcrossprod(R)
+	
+	return(kinNum)
+}
+
+.pcrCalcIBDVar <- function(G, mu, muqu, filt.idx){
 	# opposite allele frequency
 	qu <- 1 - mu
 
@@ -260,15 +317,37 @@ if(ibd.probs){
 	Gd[filt.idx] <- 0
 
 	# k0
-	k0 <- tcrossprod(IAA, Iaa) + tcrossprod(Iaa, IAA)
+	k0Num <- tcrossprod(IAA, Iaa) + tcrossprod(Iaa, IAA)
 
 	# k2
-	k2 <- tcrossprod(Gd)
+	k2Num <- tcrossprod(Gd)
 
-	return(list(k0 = k0, k2 = k2))
+	return(list(k0Num = k0Num, k2Num = k2Num))
 }
 
 
+.pcrCalcNone <- function(G, mu){
+	# index of missing values
+	filt.idx <- which(is.na(G) | is.na(mu))
+
+	# compute kinship values
+	kin <- .pcrCalcKinNone(G, mu, filt.idx)
+	return(kin)
+}
+
+.pcrCalcKinNone <- function(G, mu, filt.idx){
+	# residuals
+	R <- G - 2*mu
+
+	# set missing values to 0 (i.e. no contribution from that variant)
+	filt.idx <- which(is.na(G) | is.na(mu))	
+	R[filt.idx] <- 0
+
+	# crossprod of scaled residuals
+	kin <- tcrossprod(R)
+
+	return(kin)
+}
 
 
 
