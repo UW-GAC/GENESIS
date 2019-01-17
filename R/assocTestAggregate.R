d1dc980cc72e21cde9659ed89454c9b4ab932603
Matt <mconomos@uw.edu>
2019-01-16 16:22:24 -0800

    wip fastSKAT and fastSMMAT

--- R/assocTestAggregate.R
+++ R/assocTestAggregate.R
@@ -4,11 +4,10 @@ setMethod("assocTestAggregate",
           "SeqVarIterator",
           function(gdsobj, null.model, AF.max=1,
                    weight.beta=c(1,1), weight.user=NULL,
-                   test=c("Burden", "SKAT", "SMMAT"),
-                   # test=c("Burden", "SKAT", "SMMAT", "fastSKAT", "fastSMMAT"),
+                   test=c("Burden", "SKAT", "SMMAT", "fastSKAT", "fastSMMAT"),
                    burden.test=c("Score", "Wald"), rho=0,
                    pval.method=c("davies", "kuonen", "liu"),
-                   # neig = 100, ntrace = 500,
+                   neig = 100, ntrace = 500,
                    sparse=TRUE, verbose=TRUE) {
 
               # check argument values
@@ -194,8 +193,8 @@ setMethod("assocTestAggregate",
                       # do the test
                       assoc <- testVariantSet(null.model, G=geno, weights=weight, test=test, 
                                               burden.test=burden.test, rho=rho,
-                                              pval.method=pval.method)
-                                              # neig = neig, ntrace = ntrace)
+                                              pval.method=pval.method,
+                                              neig = neig, ntrace = ntrace)
                       res[[i]] <- cbind(res[[i]], assoc)
                   }
 
