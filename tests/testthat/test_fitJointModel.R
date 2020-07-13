fdb8f2bc0ea83d82af6266929df73345cad977b5
Adrienne Stilp <amstilp@uw.edu>
2020-07-13 14:22:40 -0700

    Add test for return types from fitJointModel

--- tests/testthat/test_fitJointModel.R
+++ tests/testthat/test_fitJointModel.R
@@ -14,6 +14,17 @@ test_that("returns expected names", {
 })
 
 test_that("returns expected types", {
+  n <- 100
+  nsnp <- 10
+  dat <- .testNullInputs(n)
+  geno <- .testGenoMatrix(n, nsnp = nsnp)
+  nullmod <- .fitNullModel(dat$y, dat$X, dat$cor.mat, verbose = FALSE)
+
+  jointmod <- .fitJointModel(nullmod, geno)
+  expect_is(jointmod, "list")
+  expect_is(jointmod$pve, "numeric")
+  expect_is(jointmod$fixef, "data.frame")
+  expect_is(jointmod$covar, "matrix")
 })
 
 test_that("checks input types", {
