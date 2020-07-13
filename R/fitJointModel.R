0eb774022772f614de65d54f9adf07b3b28d8375
Adrienne Stilp <amstilp@uw.edu>
2020-07-13 14:10:45 -0700

    Add skeleton internal function and tests

---
+++ R/fitJointModel.R
@@ -0,0 +1,10 @@
+# Inputs:
+# * nullmod: null model object without variants
+# * G: genotype matrix with variants to estimate jointly
+
+# Outputs:
+# * list with elements: pve, fixef?
+
+.jointModel <- function(nullmod, G) {  # # Check rownames/colnames match.
+
+}
---
+++ tests/testthat/test_fitJointModel.R
@@ -0,0 +1,23 @@
+context("joint model")
+library(Biobase)
+
+test_that("returns expected names", {
+})
+
+test_that("returns expected types", {
+})
+
+test_that("checks input types", {
+})
+
+test_that("checks names match", {
+})
+
+test_that("works with one snp", {
+})
+
+test_that("works with two snps", {
+})
+
+test_that("two snps in perfect LD", {
+})
