4fd51676e4fc45e86b65e9267e2f04bd6f0c01c3
Adrienne Stilp <amstilp@uw.edu>
2020-07-14 16:41:13 -0700

    Change name of function
    We're not fitting a model, so fitJointModel isn't really appropriate.
    Instead we're running a joint score test.

--- R/fitJointModel.R
+++ R/fitJointModel.R
@@ -3,15 +3,15 @@
 # * G: genotype matrix with variants to estimate jointly
 
 # Outputs:
-# * list with elements: pve, fixef?
-fitJointModel <- function(nullmod, G) {  # # Check rownames/colnames match.
+# * list with elements: pve, fixef, covar
+jointScoreTest <- function(nullmod, G) {  # # Check rownames/colnames match.
 
   # Check that all samples in null model are in the genotype matrix.
   missing_ids <- setdiff(nullmod$sample.id, rownames(G))
   if (length(missing_ids) > 0) {
     stop("missing samples in genotype matrix!")
   }
-  
+
   # Reorder samples.
   idx <- match(nullmod$sample.id, rownames(G))
   G <- G[idx, ]
