fa7d3d34625078fd4e472636e0d2cdf64203fc83
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-08-21 14:18:00 -0700

    Add function to return effect allele by variant
    We don't return the effect allele in the assoc testing results
    because indels can result in very long character strings making
    the results much larger. Use this function instead to return
    effect allele only for variants of interest.

---
+++ R/effectAllele.R
@@ -0,0 +1,35 @@
+setGeneric("effectAllele", function(gdsobj, ...) standardGeneric("effectAllele"))
+
+setMethod("effectAllele",
+          "SeqVarGDSClass",
+          function(gdsobj, variant.id=NULL) {
+              if (!is.null(variant.id)) {
+                  var.info <- applyMethod(gdsobj, variantInfo, variant=variant.id,
+                                          alleles=TRUE, expanded=TRUE)
+              } else {
+                  var.info <- variantInfo(gdsobj, alleles=TRUE, expanded=TRUE)
+              }
+              ## filt <- seqGetFilter(gdsobj)
+              ## seqSetFilter(gdsobj, variant.id=variant.id)
+              ## var.info <- variantInfo(gdsobj, alleles=TRUE, expanded=TRUE)
+              ## seqSetFilter(gdsobj, variant.sel=filt$variant.sel)
+              var.info <- var.info[,c("variant.id", "alt")]
+              names(var.info)[2] <- "effect.allele"
+              var.info
+          })
+
+setMethod("effectAllele",
+          "GenotypeData",
+          function(gdsobj, variant.id=NULL) {
+              A <- getAlleleA(gdsobj)
+              if (is.null(A)) {
+                  A <- getAlleleA(gdsobj@data)
+              }
+              var.info <- data.frame(variant.id=getSnpID(gdsobj),
+                                     effect.allele=A,
+                                     stringsAsFactors=FALSE)
+              if (!is.null(variant.id)) {
+                  var.info <- var.info[var.info$variant.id %in% variant.id,]
+              }
+              var.info
+          })
