4dd1d65e9bf48d4b659fc21c5720e0c6227b0794
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-10-06 16:00:00 -0700

    rename output of jointScoreTest to be consistent with assocTestSingle
    Joint tests are already present in GENESIS in the GxE option in
    assocTestSingle. Set the names of jointScoreTest outputs to be
    consistent.

--- R/jointScoreTest.R
+++ R/jointScoreTest.R
@@ -51,20 +51,20 @@ jointScoreTest <- function(nullmod, G) {  # # Check rownames/colnames match.
   # Create fixed effects data frame.
   fixef <- data.frame(
     Est = as.vector(beta),
-    SE = se,
+    Est.SE = se,
     stringsAsFactors = FALSE
   )
-  fixef$Stat <- fixef$Est / fixef$SE
+  fixef$Stat <- fixef$Est / fixef$Est.SE
   fixef$pval <- pchisq(fixef$Stat^2, lower.tail = FALSE, df = 1)
-  fixef$pve <- fixef$Stat^2 / nullmod$RSS0
+  fixef$PVE <- fixef$Stat^2 / nullmod$RSS0
 
   rownames(fixef) <- colnames(G)
 
   res <- list()
 
-  res$Stat.joint <- as.numeric(Stat.joint)
-  res$pval.joint <- as.numeric(pval.joint)
-  res$pve.joint <- pve.joint
+  res$Joint.Stat <- as.numeric(Stat.joint)
+  res$Joint.pval <- as.numeric(pval.joint)
+  res$Joint.PVE <- pve.joint
   res$fixef <- fixef
   res$betaCov <- betaCov
 
