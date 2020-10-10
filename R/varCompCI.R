79f7ccfcdacc6bb71c16002f33113056a72f8753
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-10-10 12:49:29 -0700

    standardize null model argument names
    use 'null.model' for all arguments that expect a null model object
    'nullmod' is only used in internal functions

--- R/varCompCI.R
+++ R/varCompCI.R
@@ -1,30 +1,30 @@
-varCompCI <- function(nullMMobj, prop=TRUE){
+varCompCI <- function(null.model, prop=TRUE){
     # check for mixed model
-    if(!(nullMMobj$family$mixedmodel)){
-        stop("nullMMobj is not a mixed model; this calculation can not be performed")
+    if(!(null.model$family$mixedmodel)){
+        stop("null.model is not a mixed model; this calculation can not be performed")
     }
 
     if(prop){
-        if(nullMMobj$hetResid){ 
+        if(null.model$hetResid){ 
             stop("Estimates of proportional variance are not supported with heterogeneous group residual variances")
         }
-        ci <- matrix(NA, nrow=length(nullMMobj$varComp), ncol=2)
-        est <- nullMMobj$varComp/sum(nullMMobj$varComp)
-        varCompCov <- nullMMobj$varCompCov
+        ci <- matrix(NA, nrow=length(null.model$varComp), ncol=2)
+        est <- null.model$varComp/sum(null.model$varComp)
+        varCompCov <- null.model$varCompCov
         varCompCov[is.na(varCompCov)] <- 0
         for(i in 1:length(est)){
-            deltaH <- rep(-nullMMobj$varComp[i]/(sum(nullMMobj$varComp)^2),length(nullMMobj$varComp))
-            deltaH[i] <- deltaH[i] + sum(nullMMobj$varComp)/(sum(nullMMobj$varComp)^2)
+            deltaH <- rep(-null.model$varComp[i]/(sum(null.model$varComp)^2),length(null.model$varComp))
+            deltaH[i] <- deltaH[i] + sum(null.model$varComp)/(sum(null.model$varComp)^2)
             varH <- as.numeric(crossprod(deltaH, crossprod(varCompCov, deltaH)))
             ci[i,] <- est[i] + sqrt(varH)*qnorm(c(0.025,0.975))
         }
-        ci[nullMMobj$zeroFLAG,] <- NA
+        ci[null.model$zeroFLAG,] <- NA
         res <- as.data.frame(cbind(est, ci))
         names(res) <- c("Proportion", "Lower 95", "Upper 95")
         
     }else{
-        ci <- nullMMobj$varComp + sqrt(diag(nullMMobj$varCompCov)) %o% qnorm(c(0.025,0.975))
-        res <- as.data.frame(cbind(nullMMobj$varComp, ci))
+        ci <- null.model$varComp + sqrt(diag(null.model$varCompCov)) %o% qnorm(c(0.025,0.975))
+        res <- as.data.frame(cbind(null.model$varComp, ci))
         names(res) <- c("Est", "Lower 95", "Upper 95")
     }
     
