faf93714fc1e487893746c04c3b4fba167633c21
Adrienne Stilp <amstilp@uw.edu>
2020-11-04 16:31:57 -0800

    Update updateNullModCond for null model restructure
    The test for this function is wrapped in a if(FALSE) block, so it
    didn't run in automated tests.

--- R/updateNullModCond.R
+++ R/updateNullModCond.R
@@ -1,8 +1,8 @@
 
-### fit a conditional model based on a previously fit model. Default does not re-estimate variance components, but it may. 
+### fit a conditional model based on a previously fit model. Default does not re-estimate variance components, but it may.
 updateNullModCond <- function(nullmod, G, covMatList = NULL,  AIREML.tol = 1e-6, max.iter = 100, drop.zeros = TRUE, verbose = TRUE){
-    
-    ## a few checks that may be transfered to wrapper function: 
+
+    ## a few checks that may be transfered to wrapper function:
     if (nullmod$hetResid & is.null(nullmod$group.idx)) stop("group indices are required for updating the null model")
 
     #if (updateVarComp){ ## this check may be pulled out for wrapper function.
@@ -10,28 +10,26 @@ updateNullModCond <- function(nullmod, G, covMatList = NULL,  AIREML.tol = 1e-6,
     if (nullmod$hetResid & is.null(nullmod$group.idx)) stop("group indices are required for updating variance components")
     #}
 
-    
+
     X = cbind(nullmod$model.matrix, G)
-    
+
     if (!nullmod$family$mixedmodel){ ## if it is not a mixed model, re-fit the model. (This includes heterogeneous residuals).
-        
-        return(.fitNullModel(nullmod$outcome, X, covMatList = NULL, 
-                             group.idx = nullmod$group.idx, family = nullmod$family$family))  	
+
+        return(.fitNullModel(nullmod$fit$outcome, X, covMatList = NULL,
+                             group.idx = nullmod$group.idx, family = nullmod$family$family))
     }
-    
-    
+
+
     ### if the function reached this far, nullmod is a mixed model!
     ### Re-fit the model with the new design matrix and start point the varComp
-    ## from the provided nullmod object. 
-    
-    new.nullmod <- .fitNullModel(nullmod$outcome, X, covMatList = covMatList, 
+    ## from the provided nullmod object.
+
+    new.nullmod <- .fitNullModel(nullmod$fit$outcome, X, covMatList = covMatList,
                                  group.idx = nullmod$group.idx, family = nullmod$family$family, start = nullmod$varComp,
                                  AIREML.tol = AIREML.tol, max.iter= max.iter, drop.zeros = drop.zeros, verbose = verbose)
-    
+
     ## add any extra slots
     extra <- setdiff(names(nullmod), names(new.nullmod))
     new.nullmod <- c(new.nullmod, nullmod[extra])
-    return(new.nullmod)  
+    return(new.nullmod)
 }
-
-
