594c17729d858a7e351773a1c83002fd7603f148
Adrienne Stilp <amstilp@uw.edu>
2020-11-04 16:31:26 -0800

    Update nullModelSubset for null model restructure
    Note there is no test for this function, so I didn't find it when
    initially changing the null model structure.

--- R/nullModelSubset.R
+++ R/nullModelSubset.R
@@ -1,19 +1,17 @@
 ## idx.exclude are indices of individuals that should be excluded (e.g. because of missing genotypes)
 nullModelSubset <- function(nullmod, idx.exclude){
-    for (v in c("sample.id", "fitted.values", "resid.marginal", "resid.condition", "workingY", "outcome")) {
-        nullmod[[v]] <- nullmod[[v]][-idx.exclude]
-    }
+    nullmod$fit <- nullmod$fix[-idx.exclude, ]
     nullmod$model.matrix <- nullmod$model.matrix[-idx.exclude,]
-    
+
     if (nullmod$family$mixedmodel){  ## n by n cholSigmaInv {
         nullmod$cholSigmaInv <- subsetCholSigmaInv(nullmod$cholSigmaInv, idx.exclude)
     }
-    
+
     if (!nullmod$family$mixedmodel & (nullmod$family$family == "gaussian")){  ## a diagonal or scalar cholSigmaInv
-        
+
         if (nullmod$hetResid)	{  ## cholSigmaInv is diagonal
             nullmod$cholSigmaInv <- nullmod$cholSigmaInv[-idx.exclude, -idx.exclude]
-        }   
+        }
     }
 }
 
@@ -29,6 +27,6 @@ subsetCholSigmaInv <- function(cholSigmaInv, chol.idx) {
         }
         cholSigmaInv <- t(chol(SigmaInv))
     }
-    
+
     cholSigmaInv
 }
