c9c39da26dcc2f4eba5e49e10233cf888ad07ea5
Adrienne Stilp <amstilp@uw.edu>
2020-11-10 16:03:31 -0800

    Fix tests for glmm null model
    The test data for the null model didn't work properly for glmm
    models; all variance components went to zero, so the function fit
    a regular logistic model instead of one with variance components.
    Fix the test data so it works properly for glmm models. Add a second
    test that explicitly tests running a regular glm model when the
    variance components go to zero.

--- tests/testthat/test_null_model_glmm.R
+++ tests/testthat/test_null_model_glmm.R
@@ -2,13 +2,43 @@ context("check null model glmm")
 
 test_that("glmm", {
     dat <- .testNullInputs(binary=TRUE)
-
-    nullmod <- .fitNullModel(dat$y, dat$X, covMatList=dat$cor.mat, family="binomial", verbose=FALSE)
+    # With the test data, all variance components go to zero, so glm is used.
+    # Create test data where that doesn't happen to test the glmm models.
+    set.seed(1); y <- sample(c(0,1), 100, replace = T)
+    set.seed(2); cov_mat <- crossprod(matrix(rnorm(100*100, sd=0.05), 100, 100))
+    nullmod <- .fitNullModel(y, dat$X, covMatList=dat$cor.mat, family="binomial", verbose=FALSE)
 
     expected_names <- c("family", "hetResid", "varComp", "varCompCov", "fixef",
-                        "betaCov", "fit", "logLik", "AIC", "model.matrix",
-                        "group.idx", "cholSigmaInv", "converged", "zeroFLAG",
-                        "RSS", "CX", "CXCXI", "RSS0")
+                        "betaCov", "fit", "logLik", "logLikR", "niter", "AIC",
+                        "model.matrix", "group.idx", "cholSigmaInv", "converged",
+                        "zeroFLAG", "RSS", "CX", "CXCXI", "RSS0")
+    expect_true(setequal(names(nullmod), expected_names))
+
+    # Check names of fit data frame.
+    expected_names <- c("outcome", "workingY", "fitted.values", "resid.marginal",
+                        "resid.conditional", "resid", "resid.cholesky")
+    expect_true(setequal(names(nullmod$fit), expected_names))
+
+    expect_equal(nullmod$family$family, "binomial")
+    expect_true(nullmod$family$mixedmodel)
+    expect_true(is(nullmod, "GENESIS.nullMixedModel"))
+    expect_false(nullmod$hetResid)
+    expect_true(nullmod$converged)
+    expect_equivalent(nullmod$fit$outcome, y)
+    expect_equivalent(nullmod$model.matrix, dat$X)
+    expect_equal(nullmod$RSS, 1)
+})
+
+test_that("glmm fit as glm", {
+    dat <- .testNullInputs(binary=TRUE)
+    # Note that when running with test data, variance components go to zero.
+    expect_message(nullmod <- .fitNullModel(dat$y, dat$X, covMatList=dat$cor.mat, family="binomial", verbose=FALSE),
+                   "using glm")
+
+    expected_names <- c("family", "hetResid", "varComp", "varCompCov", "fixef",
+                        "betaCov", "fit", "logLik", "AIC",
+                        "model.matrix", "group.idx", "cholSigmaInv", "converged",
+                        "zeroFLAG", "RSS", "CX", "CXCXI", "RSS0")
     expect_true(setequal(names(nullmod), expected_names))
 
     # Check names of fit data frame.
@@ -17,13 +47,8 @@ test_that("glmm", {
     expect_true(setequal(names(nullmod$fit), expected_names))
 
     expect_equal(nullmod$family$family, "binomial")
-    if (!nullmod$zeroFLAG) {
-        expect_true(nullmod$family$mixedmodel)
-        expect_true(is(nullmod, "GENESIS.nullMixedModel"))
-    } else {
-        expect_false(nullmod$family$mixedmodel)
-        expect_true(is(nullmod, "GENESIS.nullModel"))
-    }
+    expect_false(nullmod$family$mixedmodel)
+    expect_true(is(nullmod, "GENESIS.nullModel"))
     expect_false(nullmod$hetResid)
     expect_true(nullmod$converged)
     expect_equivalent(nullmod$fit$outcome, dat$y)
