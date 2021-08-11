569878eef2701a4acc3905b380ddb39adad98128
Stephanie M. Gogarten <sdmorris@uw.edu>
2021-08-10 17:10:13 -0700

    use SerialParam for unit tests

--- tests/testthat/test_assocTestAggregate.R
+++ tests/testthat/test_assocTestAggregate.R
@@ -3,8 +3,8 @@ library(SeqVarTools)
 library(GenomicRanges)
 library(Biobase)
 
-#BPPARAM <- BiocParallel::SerialParam()
-BPPARAM <- BiocParallel::MulticoreParam()
+BPPARAM <- BiocParallel::SerialParam()
+#BPPARAM <- BiocParallel::MulticoreParam()
 
 test_that("window", {
     svd <- .testData()
